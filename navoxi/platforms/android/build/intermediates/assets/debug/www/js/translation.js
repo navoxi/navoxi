462dfac15019f4a590f0c18a9e586a2e14b16502
Bill <bill.rousseau@outlook.com>
2016-01-24 23:43:48 +0100

    CSS basique sur vues Accueil, Navigation, Villes

--- navoxi/platforms/android/build/intermediates/assets/debug/www/js/translation.js
+++ navoxi/platforms/android/build/intermediates/assets/debug/www/js/translation.js
@@ -12,7 +12,15 @@ navoxi.config(['$translateProvider', function($translateProvider) {
     	'NAVIGATION_NEWTRIP_BUTTON': 'Nouveau trajet',
     	'NAVIGATION_HISTORY_BUTTON': 'Historique',
     	'NAVIGATION_FAVORITES_BUTTON': 'Favoris',
-    	'VILLES_TITLE': 'Villes'
+    	'CITIES_TITLE': 'Villes',
+    	'SETTINGS_TITLE': 'Préférences',
+    	'SETTINGS_HOMEDETECTION': 'Détection de mon domicile',
+    	'SETTINGS_HOMEDETECTION_BUTTON': 'Démarrer',
+    	'SETTINGS_EXITTYPE': 'Sortie de station',
+    	'SETTINGS_EXITTYPE_RADIO_AUTO': 'Automatique',
+    	'SETTINGS_EXITTYPE_RADIO_AUTO_LABEL': 'Me guider vers la sortie la plus proche',
+    	'SETTINGS_EXITTYPE_RADIO_MANUAL': 'Manuelle',
+    	'SETTINGS_EXITTYPE_RADIO_MANUAL_LABEL': 'Me laisser le choix de la sortie'
     });
     $translateProvider.translations('en', {
     	'INSTALL_BUTTON':'Add',
@@ -22,12 +30,21 @@ navoxi.config(['$translateProvider', function($translateProvider) {
     	'SETTINGS_BUTTON': 'Settings',
     	'ABOUT_BUTTON': 'About',
     	'HOME_TITLE': 'Navoxi',
+    	'HOME_BUTTON': 'Accueil',
     	'NAVIGATION_TITLE': 'Navigation',
     	'NAVIGATION_NEWTRIP_BUTTON': 'New trip',
     	'NAVIGATION_HISTORY_BUTTON': 'History',
     	'NAVIGATION_FAVORITES_BUTTON': 'Favorites',
     	'NAVIGATION_HOME_BUTTON': 'Home',
-    	'CITIES_TITLE': 'Cities'
+    	'CITIES_TITLE': 'Cities',
+    	'SETTINGS_TITLE': 'Settings',
+    	'SETTINGS_HOMEDETECTION': 'Home detection',
+    	'SETTINGS_HOMEDETECTION_BUTTON': 'Start',
+    	'SETTINGS_EXITTYPE': 'Station exit',
+    	'SETTINGS_EXITTYPE_RADIO_AUTO': 'Automatic',
+    	'SETTINGS_EXITTYPE_RADIO_AUTO_LABEL': 'Guide me towards the closest exit',
+    	'SETTINGS_EXITTYPE_RADIO_MANUAL': 'Manual',
+    	'SETTINGS_EXITTYPE_RADIO_MANUAL_LABEL': 'Let me choose my exit'
     });
     $translateProvider
     .uniformLanguageTag('bcp47')
