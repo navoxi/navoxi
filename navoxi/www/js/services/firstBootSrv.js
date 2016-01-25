aab025f648b80c03ca5713bbb75b1a104b600f8b
Bill <bill.rousseau@outlook.com>
2016-01-25 14:41:51 +0100

    Vue Réglages - Préférences

--- navoxi/www/js/services/firstBootSrv.js
+++ navoxi/www/js/services/firstBootSrv.js
@@ -7,7 +7,7 @@ navoxi.service('firstBootSrv', function() {
 		window.localStorage.setItem('stgExitType', 'automatic');
 		window.localStorage.setItem('stgVocalRate', 50);
 		window.localStorage.setItem('stgHistorySize', 5);
-		window.localStorage.setItem('stgSmartwatchNotif', 'deactivate');
+		window.localStorage.setItem('stgSmartWatchNotif', 'deactivate');
 		window.localStorage.setItem('cityParis', 'false');
 		window.localStorage.setItem('cityLyon', 'false');
 		window.localStorage.setItem('cityMarseille', 'false');
