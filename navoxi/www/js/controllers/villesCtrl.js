cd0d2def7803628a944e59e3f70ebb8ab8ceec29
Bill <bill.rousseau@outlook.com>
2016-02-05 18:12:40 +0100

    Test avec checkboxes dans la vue Villes

--- navoxi/www/js/controllers/villesCtrl.js
+++ navoxi/www/js/controllers/villesCtrl.js
@@ -1,59 +1,36 @@
 navoxi.controller('villesCtrl', ['$scope', 'nvxTools', function($scope, nvxTools) {
-	function stringToBool(val) {
+	$scope.citiesList = [];
+
+	var stringToBool = function(val) {
     	return (val + '').toLowerCase() === 'true';
+	};
+
+	for (item in window.localStorage)
+	{
+		if (item.substring(0, 4) == "city")
+		{
+			$scope.citiesList.push({
+				name: item.substr(item.indexOf('city') + 'city'.length),
+				checked: stringToBool(window.localStorage.getItem(item))
+			});
+		}
 	}
-	// $scope.citiesList = [];
-	// for (item in window.localStorage)
-	// {
-	// 	if (item.substring(0, 4) == "city")
-	// 	{
-	// 		$scope.citiesList.push({
-	// 			name: item.substr(indexOf('city') + 4),
-	// 			checked: stringToBool(item)
-	// 		});
-	// 	}
-	// }
 	$scope.goTo = function(id, path, isBack) {
 		nvxTools.goTo(id, path, isBack);
 	};
 	$scope.buttonMessage = function(city) {
-		// nvxTools.nvxAlert(window.localStorage.getItem('city'+city));
 		return nvxTools.cityButtonMessage(city);
 	};
-	// nvxTools.nvxAlert(window.sessionStorage.getItem('lastId'));
-	
-	$scope.citiesList = [
-	{
-		text: "Paris (RATP)",
-		name: "Paris",
-		checked: stringToBool(window.localStorage.getItem('cityParis'))
-	},
-	{
-		text: "Marseille (RTM)",
-		name: "Marseille",
-		checked: stringToBool(window.localStorage.getItem('cityMarseille'))
-	},
-	{
-		text: "Lyon (TCL)",
-		name: "Lyon",
-		checked: stringToBool(window.localStorage.getItem('cityLyon'))
-	}];
-	// $scope.Paris = window.localStorage.getItem('cityParis');
-	// $scope.Marseille = window.localStorage.getItem('cityMarseille');
-	// $scope.Lyon = window.localStorage.getItem('cityLyon');
 	$scope.toggleButton = function(city) {
-		$scope[city] = !$scope[city];
 		if (window.localStorage.getItem('city'+city) == 'true')
 		{
 			window.localStorage.setItem('city'+city, 'false');
 			return nvxTools.cityButtonMessage(city);
 		}
-		else if (window.localStorage.getItem('city'+city) == 'false')
+		if (window.localStorage.getItem('city'+city) == 'false')
 		{
 			window.localStorage.setItem('city'+city, 'true');
 			return nvxTools.cityButtonMessage(city);
 		}
-		else
-			nvxTools.nvxAlert(window.localStorage.getItem('city'+city));
 	};
 }]);
